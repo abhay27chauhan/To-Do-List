b670880804c2bcefd212515567167be610f8a2ca
Abhay <chauhan27.abhay@gmail.com>
2020-06-17 20:41:57 +0530

    update

--- index.js
+++ index.js
@@ -142,11 +142,9 @@ app.get("/:customListName", function(req,res){
 //     res.render('list', {listType: "Work List", addListItems: workItems})
 // })
 
-let port = process.nextTick.PORT;
+let port = process.env.PORT;
 if(port == null || port == ""){
     port = 3000;
 }
 
-app.listen(port, function(){
-    console.log("Server is listening on port 3000");
-})
+app.listen(port);
