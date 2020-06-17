ccf4207d0e6dbf468629f9d2c95de37a426130cf
Abhay <chauhan27.abhay@gmail.com>
2020-06-17 20:33:45 +0530

    add gitignore procfile

--- index.js
+++ index.js
@@ -142,6 +142,11 @@ app.get("/:customListName", function(req,res){
 //     res.render('list', {listType: "Work List", addListItems: workItems})
 // })
 
-app.listen(3000, function(){
+let port = process.nextTick.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+
+app.listen(port, function(){
     console.log("Server is listening on port 3000");
 })
